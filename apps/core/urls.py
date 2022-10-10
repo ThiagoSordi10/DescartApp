e0cb02b5762314f30e3a8e1449f3b93162d96e7e
Guilherme Pretto <62142257+GuilhermePretto@users.noreply.github.com>
2022-10-09 22:09:14 -0300

    feat: login

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import SignUpUserView
+from .views import LoginUserView 
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    #path('login/', login_view, name="login"),
+    path('login/', LoginUserView.as_view(), name="login"),
     path('signup/', SignUpUserView.as_view(), name="signup"),
     #path("logout/", LogoutView.as_view(), name="logout")
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,19 +1,23 @@
 # Create your views here.
+from symbol import import_as_name
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.forms import BaseModelForm
 from django.http.response import HttpResponse
+from django.http.request import HttpRequest
 from django.http import HttpResponseRedirect
+from django.contrib.auth import login as auth_login
 from typing import Any, Dict
 from .forms import SignUpForm
+from .forms import LoginForm
 
 
 class SignUpUserView(CreateView):
     form_class = SignUpForm
     template_name: str = 'user/signup.html'
-    
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
@@ -36,3 +40,20 @@ class SignUpUserView(CreateView):
     def form_invalid(self, form: BaseModelForm) -> HttpResponse:
         self.request.session['msg'] = 'Form is invalid'
         return HttpResponseRedirect(reverse_lazy('signup'))
+
+class LoginUserView(CreateView):
+    form_class = LoginForm
+    template_name: str = 'user/login.html'
+
+    def get(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        if not request.user.is_anonymous:
+            return HttpResponseRedirect(reverse_lazy(settings.LOGIN_REDIRECT_URL))
+        return super(LoginUserView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        """Login"""
+        user = form.save(commit = False)
+        auth_login(self.request, user)
+
+        sucess_url = self.get_success_url()
+        return HttpResponseRedirect(sucess_url)
