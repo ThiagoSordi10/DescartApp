a28d58cd68d2da655c08c626025767e5fe7c801e
smfinkler <49410834+smfinkler@users.noreply.github.com>
2022-10-17 22:52:48 -0300

    fix: url name

--- apps/collect/urls.py
+++ apps/collect/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('demand/create/', DemandCreateView.as_view(), name="create_demand"),
     path('demand/list/', DemandListView.as_view(), name="list_demand"),
     path('demand/update/<int:pk>', DemandUpdateView.as_view(), name="update_demand"),
-    path('demand/update/<int:pk>/addresses', DemandAddressesView.as_view(), name="add_address"),
+    path('demand/update/<int:pk>/addresses', DemandAddressesView.as_view(), name="demand_address"),
 ]
